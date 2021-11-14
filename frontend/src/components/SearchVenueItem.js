814829f9e2b63bd92dae104f64ac73210f164dd1
Ryan Oostland <oostlandryan16@gmail.com>
2021-11-14 14:02:42 -0500

    Remove vid, make button redirect to venue page

--- frontend/src/components/SearchVenueItem.js
+++ frontend/src/components/SearchVenueItem.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./SearchVenueItem.css";
+import { useHistory } from "react-router";
 import randomvenueimg from "./randomweddingvenue.jpg";
 
 function SearchVenueItem({ vid, name, city, state, zipcode }) {
+  var history = useHistory();
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={randomvenueimg} />
 
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{name}</p>
-        <p className="checkoutProduct__price">
-          <small>{vid}</small>
-        </p>
         <p className="checkoutProduct__price">
           <small>{city}</small>
         </p>
@@ -21,7 +20,7 @@ function SearchVenueItem({ vid, name, city, state, zipcode }) {
         <p className="checkoutProduct__price">
           <small>{zipcode}</small>
         </p>
-        <button>More Info>></button>
+        <button onClick={() => {history.push(`/veune/${vid}`)}}>More Info>></button>
       </div>
     </div>
   );
